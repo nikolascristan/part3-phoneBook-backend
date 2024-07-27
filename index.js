f227f2b9f26131822d12898adc196ded4d26d2dd
nikolascristan <nikolascristan@gmail.com>
2024-07-27 16:33:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
-//app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 function areTheseObjectsEqual(first, second) {
   "use strict";
